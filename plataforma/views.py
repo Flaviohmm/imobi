5f5b41a8f80ccd3cbb5a5f41e6c4b96b1b02bd4a
Flaviohmm <fhenrique609@gmail.com>
2022-01-20 10:43:38 -0300

    Add Models no Admin e Criando a home.html com estilização

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Imovel
 
 # Create your views here.
 
 
 @login_required(login_url='/auth/logar')
 def home(request):
-    return HttpResponse('home')
+    imoveis = Imovel.objects.all()
+    return render(request, 'home.html', {'imoveis': imoveis})
