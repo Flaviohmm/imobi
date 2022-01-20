72d16484ecdf46bb2a0b4ad8ce9d7290a91f2cbb
Flaviohmm <fhenrique609@gmail.com>
2022-01-20 12:59:05 -0300

    Lógica para filtrar os imoveis

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Imovel
+from .models import Imovel, Cidade
 
 # Create your views here.
 
 
 @login_required(login_url='/auth/logar')
 def home(request):
-    imoveis = Imovel.objects.all()
-    return render(request, 'home.html', {'imoveis': imoveis})
+    preco_minimo = request.GET.get('preco_minimo')
+    preco_maximo = request.GET.get('preco_maximo')
+    cidade = request.GET.get('cidade')
+    tipo = request.GET.get('tipo')
+    cidades = Cidade.objects.all()
+    if preco_minimo or preco_maximo or cidade or tipo:
+        if not preco_minimo:
+            preco_minimo = 0
+        if not preco_maximo:
+            preco_maximo = 999999999
+        if not tipo:
+            tipo = ['A', 'C']
+        
+        imoveis = Imovel.objects.filter(valor__gte=preco_minimo)\
+                    .filter(valor__lte=preco_maximo)\
+                    .filter(tipo_imovel__in=tipo)\
+                    .filter(cidade=cidade)
+    else:
+        imoveis = Imovel.objects.all()
+
+    return render(request, 'home.html', {'imoveis': imoveis, 'cidades': cidades})
